ff932cb790e1cc7a6e6e5814a36e682e2845ea21
junnl <haitanga1421@163.com>
2021-01-19 20:03:35 +0800

    feat db

--- models/from_surfing.py
+++ models/from_surfing.py
@@ -1,15 +1,19 @@
 from surfing.data.view.basic_models import FOFInfo as SurfingFOFInfo
+from surfing.data.view.basic_models import HedgeFundNAV as SurfingHedgeFundNAV
 from surfing.data.view.basic_models import FOFScaleAlteration as SurfingFOFScaleAlteration
 from surfing.data.view.basic_models import FOFAssetAllocation as SurfingFOFAssetAllocation
 from surfing.data.view.basic_models import FOFManually as SurfingFOFManually
 from surfing.data.view.basic_models import FOFInvestorPosition as SurfingFOFInvestorPosition
-from surfing.data.view.basic_models import StockTag as SurfingStockTag
 from surfing.data.view.basic_models import HedgeFundInfo as SurfingHedgeFundInfo
-from surfing.data.view.basic_models import HedgeFundNAV as SurfingHedgeFundNAV
 from surfing.data.view.derived_models import FOFPosition as SurfingFOFPosition
 from surfing.data.view.derived_models import FOFNav as SurfingFOFNav
 
 from bases.dbwrapper import BaseModel
+
+
+class HedgeFundNAV(SurfingHedgeFundNAV, BaseModel):
+    __bind_key__ = 'basic'
+    # __table_args__ = {'extend_existing': True}
 
 
 class FOFInfo(SurfingFOFInfo, BaseModel):
@@ -38,11 +42,6 @@ class FOFInvestorPosition(SurfingFOFInvestorPosition, BaseModel):
 
 
 class HedgeFundInfo(SurfingHedgeFundInfo, BaseModel):
-    __bind_key__ = 'basic'
-    __table_args__ = {'extend_existing': True}
-
-
-class HedgeFundNAV(SurfingHedgeFundNAV, BaseModel):
     __bind_key__ = 'basic'
     __table_args__ = {'extend_existing': True}
 
