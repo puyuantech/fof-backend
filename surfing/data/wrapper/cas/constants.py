e8a0684b0bd0ace4155dd8c3a65d2e32852197ef
junnl <haitanga1421@163.com>
2021-01-21 16:34:37 +0800

    feat db

--- surfing/data/wrapper/cas/constants.py
+++
@@ -1,5 +0,0 @@
-from ....util.config import SurfingConfigurator
-
-# key space
-cas_default_key_space = SurfingConfigurator().get_cassandra_setting().default_key_space
-CAS_KEY_SPACE = 'surfing' if not cas_default_key_space else cas_default_key_space
