f5cbbfe67e6dfda48d3e09c80ac9f8c6ca821a34
junnl <haitanga1421@163.com>
2021-01-27 11:04:23 +0800

    [feat] wx

--- apps/wx/__init__.py
+++ apps/wx/__init__.py
@@ -8,6 +8,6 @@ api = Api(blu)
 api.add_resource(WX, '')
 api.add_resource(WXBindUser, '/user')
 api.add_resource(WxLoginAPI, '/official_login')
-api.add_resource(WxLoginAPI, '/bind_mobile')
+api.add_resource(WXBindMobile, '/bind_mobile')
 api.add_resource(OfficeAPPID, '/office_app_id')
 
