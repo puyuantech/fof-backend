c4713a61136d49b8b5e87b7c101a0b4df7b91b80
junnl <haitanga1421@163.com>
2021-01-27 11:25:55 +0800

    [feat] wx

--- apps/wx/constants.py
+++ apps/wx/constants.py
@@ -1,4 +1,4 @@
 
 
-WX_REDIRECT_URL = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=${}&redirect_uri=${}&' \
+WX_REDIRECT_URL = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid={}&redirect_uri={}&' \
                   'response_type=code&scope=snsapi_userinfo&state=1'
