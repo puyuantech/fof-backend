5c5528eb4dbb272487221a39acc364777e9d78c3
junnl <haitanga1421@163.com>
2021-01-27 10:48:02 +0800

    [feat] wx

--- apps/wx/libs.py
+++ apps/wx/libs.py
@@ -2,8 +2,10 @@ from flask import g, current_app, request
 from models import WeChatUnionID
 from bases.exceptions import VerifyError
 from bases.globals import settings
-
+from extensions.wx.reply import TextMsg, ImageMsg
 from extensions.wx.msg_crypt.msg_crypt import WXBizMsgCrypt
+
+from .constants import WX_REDIRECT_URL
 
 
 def check_we_chat_user_exist(union_id):
@@ -11,7 +13,8 @@ def check_we_chat_user_exist(union_id):
         union_id=union_id,
     ).first()
     if wx:
-        raise VerifyError('此微信已绑定账号！')
+        return True, wx
+    return False, None
 
 
 def bind_we_chat_user(union_id, open_id):
@@ -57,3 +60,22 @@ def encode_wx_msg(ret_xml, nonce):
         raise VerifyError('加密失败！')
 
     return xml_data
+
+
+def wx_text(rec_msg, nonce):
+    input_content = rec_msg.find('Content')
+    ret_xml = TextMsg(rec_msg.FromUserName, rec_msg.ToUserName, input_content).results()
+    ret = encode_wx_msg(ret_xml, nonce)
+    return ret
+
+
+def wx_event(rec_msg):
+    msg_event = rec_msg.find('Event')
+    if msg_event == 'SCAN':
+        return 'success'
+    elif msg_event == 'subscribe':
+        return 'success'
+
+    return 'success'
+
+
--- apps/wx/view.py
+++ apps/wx/view.py
@@ -1,17 +1,18 @@
 import hashlib
+import traceback
 from xml.etree import ElementTree as ET
 
 from flask import g, make_response, request, current_app
 from bases.viewhandler import ApiViewHandler
-from bases.globals import settings
+from bases.globals import settings, db
 from bases.exceptions import VerifyError
 from utils.decorators import params_required, login_required
 from extensions.wx.union_manager import WXUnionManager
 from extensions.wx.receive import Msg
-from extensions.wx.reply import TextMsg, ImageMsg
-from extensions.wx.msg_crypt.msg_crypt import WXBizMsgCrypt
+from models import User, WeChatUnionID, Token
 
-from .libs import check_we_chat_user_exist, bind_we_chat_user, decode_wx_msg, encode_wx_msg
+from .libs import check_we_chat_user_exist, bind_we_chat_user, decode_wx_msg, encode_wx_msg, \
+    wx_text, wx_event
 
 
 class WX(ApiViewHandler):
@@ -46,18 +47,55 @@ class WX(ApiViewHandler):
         rec_msg = Msg(xml_data)
 
         if rec_msg.MsgType == 'text':
-            input_content = rec_msg.find('Content')
-            ret_xml = TextMsg(rec_msg.FromUserName, rec_msg.ToUserName, input_content).results()
-            ret = encode_wx_msg(ret_xml, self.input.nonce)
+            ret = wx_text(rec_msg, self.input.nonce)
         elif rec_msg.MsgType == 'event':
-            pass
+            ret = wx_event(rec_msg)
         elif rec_msg.MsgType == 'image':
-            pass
+            ret = 'success'
         else:
-            ret = '您好'
+            ret = 'success'
 
         current_app.logger.info(ret)
-        return make_response(ret)
+        return make_response('success')
+
+
+class WxLoginAPI(ApiViewHandler):
+
+    @params_required(*['code'])
+    def post(self):
+        try:
+            union_info = WXUnionManager.get_union_id('fof', self.input.code)
+        except Exception:
+            raise VerifyError(traceback.format_exc())
+
+        union_id, open_id = union_info['unionid'], union_info.get('openid')
+        is_exists, wx_user = check_we_chat_user_exist(union_id)
+
+        try:
+            if is_exists:
+                user = wx_user.user
+            else:
+                user = User.create(
+                    nick_name=union_info.get('nickname', ''),
+                    is_staff=False,
+                )
+                user.avatar_url = union_info.get('headimgurl')
+                WeChatUnionID.create(
+                    union_id=union_id,
+                    user_id=user.id,
+                    open_id=open_id,
+                )
+                db.session.commit()
+
+            user_dict = user.to_dict()
+            token_dict = Token.generate_token(user.id)
+            return {
+                'user': user_dict,
+                'token': token_dict,
+            }
+        except Exception as e:
+            current_app.logger.error('[wx_login] 查询用户数据库失败 (err_msg){}'.format(traceback.format_exc()))
+            raise VerifyError('查询用户数据库失败，(err_msg){}'.format(traceback.format_exc()))
 
 
 class WXBindUser(ApiViewHandler):
@@ -83,10 +121,13 @@ class WXBindUser(ApiViewHandler):
 
 class WXBindMobile(ApiViewHandler):
 
-    def get(self):
-        return {
-            'app_id': settings['WX']['apps']['fof']['app_id'],
-        }
+    @params_required(*['mobile', 'mobile_code'])
+    @login_required
+    def post(self):
+        if g.user.mobile:
+            raise VerifyError('您已经绑定过手机号！')
+
+        return
 
 
 class OfficeAPPID(ApiViewHandler):
