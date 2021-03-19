bb4792addfae0dc70fff591499226d07908dc6f6
douchen <playgoobug@gmail.com>
2021-03-19 11:24:29 +0800

    fill investor contracts

--- apps/investor/libs.py
+++ apps/investor/libs.py
@@ -1,9 +1,11 @@
 
 from flask import g
+from typing import List
 
+from bases.constants import ContractStatus
 from extensions.haifeng.fof_template import FOFTemplate
 from extensions.s3.pdf_store import PdfStore
-from models import InvestorCertification, FOFInfo
+from models import FOFInfo, InvestorCertification
 
 
 def fill_investor_contract(investor_contract: dict):
@@ -20,6 +22,26 @@ def fill_investor_contract(investor_contract: dict):
         'fof_name': fof_info.fof_name,
     })
     return investor_contract
+
+
+def fill_investor_contracts(manager_id, investor_contracts: List[dict]):
+    fofs = FOFInfo.filter_by_query(manager_id=manager_id).all()
+    fofs = {
+        fof.fof_id: {'fof_name': fof.fof_name, 'risk_type': fof.risk_type}
+        for fof in fofs
+    }
+
+    for investor_contract in investor_contracts:
+        investor_contract.update(fofs.pop(investor_contract['fof_id']))
+
+    for fof_id, fof_info in fofs.items():
+        investor_contracts.append({
+            'contract_status': ContractStatus.UNBOOKED,
+            'fof_id': fof_id,
+            **fof_info,
+        })
+
+    return investor_contracts
 
 
 def get_contract_url_key(contract_id, manager_id):
