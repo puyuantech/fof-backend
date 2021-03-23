f96028a313ffb5442ed25ab742d86c104a449843
douchen <playgoobug@gmail.com>
2021-03-23 13:37:36 +0800

    add macro markets menu/ret

--- apps/markets/__init__.py
+++ apps/markets/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 from .view import IndexListAPI, IndexAPI, IndexDetailAPI, IndexSingleChangeAPI
+from .view_macro import MacroMenuAPI, MacroRetAPI
 
 blu = Blueprint('{}_blu'.format(__name__), __name__, url_prefix='/api/v1/market')
 api = Api(blu)
@@ -10,3 +11,5 @@ api.add_resource(IndexAPI, '/<int:_id>')
 api.add_resource(IndexDetailAPI, '/detail/<int:_id>')
 api.add_resource(IndexSingleChangeAPI, '/single_change')
 
+api.add_resource(MacroMenuAPI, '/macro/menu')
+api.add_resource(MacroRetAPI, '/macro/ret')
