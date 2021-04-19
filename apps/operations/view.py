ddd0e6e80d313d1b07caaf6f2f9191bc40f6c8a8
junnl <haitanga1421@163.com>
2021-04-19 15:08:26 +0800

    [feat] investor operation

--- apps/operations/view.py
+++ apps/operations/view.py
@@ -1,10 +1,10 @@
-from flask import g
+from flask import g, request
 from bases.viewhandler import ApiViewHandler
 from bases.constants import StuffEnum
 from bases.globals import db
-from utils.decorators import admin_login_required
+from utils.decorators import admin_login_required, login_required, params_required
 from utils.helper import generate_sql_pagination
-from models import Operation, User
+from models import Operation, User, InvestorOperation, UnitMap
 
 from sqlalchemy import distinct
 
@@ -38,3 +38,46 @@ class OperationsListAPI(ApiViewHandler):
         results = db.session.query(distinct(Operation.action)).all()
         data = [i[0] for i in results]
         return data
+
+
+class InvestorOperationAPI(ApiViewHandler):
+
+    @admin_login_required([StuffEnum.ADMIN, StuffEnum.FUND_MANAGER, StuffEnum.OPE_MANAGER])
+    def get(self):
+        p = generate_sql_pagination()
+
+        query = InvestorOperation.filter_by_query(
+            manager_id=g.token.manager_id,
+        )
+        data = p.paginate(
+            query,
+            equal_filter=[Operation.user_id, Operation.action, Operation.investor_id],
+            range_filter=[Operation.create_time],
+        )
+
+        for i in data['results']:
+            u = UnitMap.filter_by_query(
+                investor_id=i['investor_id'],
+                manager_id=g.token.manager_id,
+            ).first()
+            if u:
+                i['mobile'] = u.mobile
+                i['name'] = u.name
+        return data
+
+    @params_required(*['request_data', 'investor_id', 'action'])
+    @login_required
+    def post(self):
+        remote_addr = request.headers.get('X-Real-Ip') if request.headers.get('X-Real-Ip') else '0.0.0.0'
+        InvestorOperation.create(
+            investor_id=self.input.investor_id,
+            user_id=g.user.id,
+            manager_id=g.token.manager_id,
+            ip=str(remote_addr),
+            action=self.input.action,
+            url=str(request.url),
+            method=request.method,
+            request_data=self.input.request_data,
+            response_data='',
+            response_status=None,
+        )
--- models/operations.py
+++ models/operations.py
@@ -4,9 +4,28 @@ from sqlalchemy.dialects.mysql import DOUBLE
 
 class Operation(BaseModel):
     """
-    用户操作记录
+    管理者操作记录
     """
     __tablename__ = 'operations'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)            # 编号
+    user_id = db.Column(db.Integer)                                             #
+    investor_id = db.Column(db.String(32))                                      # 投资者ID
+    ip = db.Column(db.String(31))                                               # IP
+    action = db.Column(db.String(31))                                           # 操作
+    url = db.Column(db.TEXT)                                                    # 请求地址
+    method = db.Column(db.String(15))                                           # 请求方法
+    request_data = db.Column(db.TEXT)                                           # 请求体
+    response_data = db.Column(db.TEXT)                                          # 返回数据
+    response_status = db.Column(db.Integer)                                     # 返回状态码
+    manager_id = db.Column(db.String(32))                                       # 管理者ID
+
+
+class InvestorOperation(BaseModel):
+    """
+    投资者操作记录
+    """
+    __tablename__ = 'operations_investor'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)            # 编号
     user_id = db.Column(db.Integer)                                             #
